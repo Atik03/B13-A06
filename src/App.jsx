35dfb1b80f8d765718e9da4650f1b3fdfb661fc3
Atik03 <atikshaharia03@gmail.com>
2026-03-31 10:46:58 +0600

    create react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500"> hello </h1>
+
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  );
+}
+
+export default App;
